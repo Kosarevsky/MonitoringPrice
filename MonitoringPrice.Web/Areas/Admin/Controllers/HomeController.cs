662f9d4cc4ad0a4c7ab97970c951d0ccd62a01c6
Armada <armada@tut.by>
2022-06-25 19:09:52 +0300

    Fix Manufacturer

--- MonitoringPrice.Web/Areas/Admin/Controllers/HomeController.cs
+++ MonitoringPrice.Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,28 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MonitoringPrice.Services.Interfaces;
-using MonitoringPrice.Services.Models;
 
 namespace MonitoringPrice.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
-        private readonly IManufacturerService _manufacturerService;
-
-        public HomeController(IManufacturerService manufacturerService)
+        public IActionResult Index()
         {
-            _manufacturerService = manufacturerService;
-        }
-        public async Task<IActionResult> Index()
-        {
-            IEnumerable<ManufacturerModel> bbb = await _manufacturerService.GetAllManufacturerFromApi();
-            return View(bbb);
+            return View();
         }
 
-        public async Task<ActionResult> Edit(int id)
-        {
-            var model = id == default ? new ManufacturerModel() : await _manufacturerService.GetManufactyrerById(id);
-            return View(model);
-        }
     }
 }
