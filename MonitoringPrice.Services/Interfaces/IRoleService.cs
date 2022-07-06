fd9e3d00bc8b7bb33ef2ea34d2465e6505eeff23
Armada <armada@tut.by>
2022-07-07 01:03:58 +0300

    Fix Obsolete

--- MonitoringPrice.Services/Interfaces/IRoleService.cs
+++
@@ -1,9 +0,0 @@
-﻿using MonitoringPrice.Services.Models;
-
-namespace MonitoringPrice.Services.Interfaces
-{
-    public interface IRoleService
-    {
-        Task<RoleModel> GetRoleByName(string name);
-    }
-}
